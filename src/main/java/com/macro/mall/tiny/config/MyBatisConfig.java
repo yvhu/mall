65e7fbb7a582c23bf0b40eaeb6311e587db8e9ea
huchunyu <hu_chunyv@163.com>
2022-04-21 22:53:45 +0800

    first commit

---
+++ src/main/java/com/macro/mall/tiny/config/MyBatisConfig.java
@@ -0,0 +1,9 @@
+package com.macro.mall.tiny.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.macro.mall.tiny.mbg.mapper")
+public class MyBatisConfig {
+}
---
+++ src/main/java/com/macro/mall/tiny/mbg/CommentGenerator.java
@@ -0,0 +1,4 @@
+package com.macro.mall.tiny.mbg;
+
+public class CommentGenerator {
+}
